ec8a036d0fafb6ea7ad1ab4b2bd75943b1cce6b5
freepascal <magicalmoon17@gmail.com>
2016-03-16 16:55:46 +0700

    update circleCi

---
+++ config/testing/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+    'default'       => 'mysql',
+    'connections'   => array(
+        'mysql'     => array(
+            'driver'    => 'mysql',
+            'host'      => getenv('DB_URL'),
+            'port'      => 3306,
+            'database'  => getenv('DB_NAME'),
+            'username'  => getenv('DB_USER'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => ''
+    )
+);
+
+?>
